49e7478f5558d33045f96c7597a31b6a369d02ba
Sorasuk Thongkum <sorasukt01@gmail.com>
2026-08-26 17:46:11 +0700

    Load learning workflow styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
 import './index.css'
 import './ui-polish.css'
+import './learning-workflows.css'
 import App from './App.tsx'
 import Auth0ProviderWithConfig from './auth/Auth0ProviderWithConfig.tsx'
 
